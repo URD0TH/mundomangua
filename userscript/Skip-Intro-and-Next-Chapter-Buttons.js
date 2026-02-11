08e4b4f300f6c74f13c6e3e8a8c2b9f25c17d8ac
URD0TH <urd0th@gmail.com>
2026-02-11 04:26:56 -0300

    Update version number in userscript metadata

--- userscript/Skip-Intro-and-Next-Chapter-Buttons.js
+++ userscript/Skip-Intro-and-Next-Chapter-Buttons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Skip Intro & Next Chapter Buttons
 // @namespace    https://github.com/URD0TH/mundomangua
-// @version      0.4.11
+// @version      0.4.11.hf01
 // @description  Añade botones para saltar la intro y para ir al siguiente capítulo.
 // @author       Mundomangua
 // @match        https://www.mundodonghua.com/*
